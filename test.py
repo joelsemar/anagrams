212ff9c5a501d10664cc41166b05a0be2ff9330b
Joel Semar <jsemar@homeaway.com>
2015-07-28 15:29:06 -0500

    added README

--- test.py
+++ test.py
@@ -7,8 +7,13 @@ def test_is_anagram():
     assert is_anagram("rob", "orb")
     assert not is_anagram("Joel Semar", random_string(12))
     assert not is_anagram("fantastic", random_string(12))
+    print "is_anagram seems to work"
 
 
 def random_string(size):
     alpha =  "abcdefghijklmnopqrstuvwxyz"
     return "".join([random.choice(alpha) for i in range(size)])
+
+
+if __name__ == "__main__":
+    test_is_anagram()
